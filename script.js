818bc089872c6e18128fb909c702123e130b66d3
Saurabh Bhajibhakare <saurabh.bhajibhakare@ifmr.ac.in>
2020-09-14 09:44:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ slider.oninput = function() {
 var slider2 = document.getElementById("range2");
 var output2 = document.getElementById("demo2");
 
-output2.innerHTML = slider.value;
+output2.innerHTML = slider2.value;
 
 slider2.oninput = function() {
   output2.innerHTML = this.value;
@@ -21,7 +21,7 @@ slider2.oninput = function() {
 var slider3 = document.getElementById("range3");
 var output3 = document.getElementById("demo3");
 
-output3.innerHTML = slider.value;
+output3.innerHTML = slider3.value;
 
 slider3.oninput = function() {
   output3.innerHTML = this.value;
@@ -30,7 +30,7 @@ slider3.oninput = function() {
 var slider4 = document.getElementById("range4");
 var output4 = document.getElementById("demo4");
 
-output4.innerHTML = slider.value;
+output4.innerHTML = slider4.value;
 
 slider4.oninput = function() {
   output4.innerHTML = this.value;
@@ -39,8 +39,40 @@ slider4.oninput = function() {
 var slider5 = document.getElementById("range5");
 var output5 = document.getElementById("demo5");
 
-output5.innerHTML = slider.value;
+output5.innerHTML = slider5.value;
 
 slider5.oninput = function() {
   output5.innerHTML = this.value;
+}
+
+var slider6 = document.getElementById("range6");
+var output6 = document.getElementById("demo6");
+
+output6.innerHTML = slider6.value;
+
+slider6.oninput = function() {
+  output6.innerHTML = this.value;
+}
+
+var slider7 = document.getElementById("range7");
+var output7 = document.getElementById("demo7");
+
+output7.innerHTML = slider7.value;
+
+slider7.oninput = function() {
+  output7.innerHTML = this.value;
+}
+
+var slider8 = document.getElementById("range8");
+var output8 = document.getElementById("demo8");
+
+output8.innerHTML = slider8.value;
+
+slider8.oninput = function() {
+  output8.innerHTML = this.value;
+}
+
+
+function myFunction() {
+  alert("The form was submitted");
 }
